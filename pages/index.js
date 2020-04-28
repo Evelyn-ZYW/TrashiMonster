d11dfdfd013e44b0082aae1b2d863b7619dc3661
Yunyun <yunyun@pengyunyunde-MacBook-Pro.local>
2020-04-28 13:14:10 -0700

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,13 @@ function ClickIndex(){
 
 const Index = () => <div>Index
   <p />
-  
-  <Link href="Duration"><a>Duration</a></Link>
-   <Link href="Home"><a>Home Page</a></Link>
-   <Link href="Location"><a>location</a></Link>
-   <Link href="Category"><a>Category</a></Link>
-   <Link href="Result"><a>Result</a></Link>
-   <Link href="Tips"><a>Tips</a></Link>
-   <Link href="Summary"><a>Summary</a></Link>
+   <Link href="Home"><a>Home Page</a></Link><br/>
+   <Link href="Duration"><a>Duration</a></Link> <br/>
+   <Link href="Location"><a>location</a></Link><br/>
+   <Link href="Category"><a>Category</a></Link><br/>
+   <Link href="Result"><a>Result</a></Link><br/>
+   <Link href="Tips"><a>Tips</a></Link><br/>
+   <Link href="Summary"><a>Summary</a></Link><br/>
    <Link href="Tutorial"><a>Tutorial</a></Link>
 
 </div>
