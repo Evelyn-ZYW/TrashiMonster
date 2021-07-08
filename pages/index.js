c2397275bc49977d34a8f6856d3e88ce4115de6a
= <=>
2021-07-07 17:10:46 -0700

    some changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import './index.css';
 import Router from 'next/router';
+import Home from '../pages/Home';
 
 function ClickIndex(){
   
@@ -8,18 +9,20 @@ function ClickIndex(){
       Router.push("/Location");
   }, 1000)
 }
-
-
-const Index = () => <div>Index
-  <br />
-   <Link href="Home"><a>Home Page</a></Link><br/>
-   <Link href="Duration"><a>Duration</a></Link> <br/>
-   <Link href="Location"><a>location</a></Link><br/>
-   <Link href="Category"><a>Category</a></Link><br/>
-   <Link href="Result"><a>Result</a></Link><br/>
-   <Link href="Tips"><a>Tips</a></Link><br/>
-   <Link href="Summary"><a>Summary</a></Link><br/>
-   <Link href="Tutorial"><a>Tutorial</a></Link>
+const Index = () => <div>
+  <Home />
 </div>
+
+// const Index = () => <div>Index
+//   <br />
+//    <Link href="Home"><a>Home Page</a></Link><br/>
+//    <Link href="Duration"><a>Duration</a></Link> <br/>
+//    <Link href="Location"><a>location</a></Link><br/>
+//    <Link href="Category"><a>Category</a></Link><br/>
+//    <Link href="Result"><a>Result</a></Link><br/>
+//    <Link href="Tips"><a>Tips</a></Link><br/>
+//    <Link href="Summary"><a>Summary</a></Link><br/>
+//    <Link href="Tutorial"><a>Tutorial</a></Link>
+// </div>
  
 export default Index;
